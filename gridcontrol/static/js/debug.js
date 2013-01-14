f97acaf8a4b311fe1dac6181c3ec209cef4d4a65
shu zOMG chen <shu.chen@freelancedreams.com>
2013-01-14 23:37:16 +0000

    underscoring some stuff

--- gridcontrol/static/js/debug.js
+++ gridcontrol/static/js/debug.js
@@ -3,14 +3,14 @@
 
 	var mapping, source, parsed;
 
-	var bind = function (el, query) {
-		el.mouseover(function() {
+	var bind = function ($el, query) {
+		$el.mouseover(function() {
 			$(query).addClass('source_highlighted');
 		});
-		el.mouseout(function() {
+		$el.mouseout(function() {
 			$(query).removeClass('source_highlighted');
 		});
-		el.click(function() {
+		$el.click(function() {
 			$('.source_line').removeClass('source_clicked');
 			$(query).addClass('source_clicked');
 		});
@@ -21,20 +21,20 @@
 		var query = '.' + css;
 		var line_num = _.str.pad(n, (""+all.length).length, '0', 'left');
 
-		var el = $('<div class="source_line ' + css + '" />');
-		el.append($('<span class="line_num" />').text(line_num));
-		el.append(root.document.createTextNode(line));
+		var $el = $('<div class="source_line ' + css + '" />');
+		$el.append($('<span class="line_num" />').text(line_num));
+		$el.append(root.document.createTextNode(line));
 
-		bind(el, query);
+		bind($el, query);
 
-		return el;
+		return $el;
 	};
 
 	var make_stack = function (frame) {
 		var n = mapping[frame[1] - 1];
 		var css = 'line_' + n;
 
-		var el = $('<div class="source_line ' + css + '"/>');
+		var $el = $('<div class="source_line ' + css + '"/>');
 
 		var src = source[n].split(' ');
 		var last = src[src.length - 1];
@@ -42,63 +42,59 @@
 			frame.push(last);
 		}
 
-		el.text('[' + frame.join(', ') + ']');
+		$el.text('[' + frame.join(', ') + ']');
 
-		bind(el, '.' + css);
+		bind($el, '.' + css);
 
-		return el;
+		return $el;
 	};
 
 	var make_current = function (pos) {
 		var css = 'line_' + mapping[pos - 1];
-		var el = $('<div class="source_line current_line ' + css + '"/>');
+		var $el = $('<div class="source_line current_line ' + css + '"/>');
 
 		var frame = ['CURRENT', pos];
-		el.text('[' + frame.join(', ') + ']');
+		$el.text('[' + frame.join(', ') + ']');
 
 		var query = '.' + css;
 		$(query).addClass('current_line');
-		bind(el, query);
+		bind($el, query);
 
-		return el;
+		return $el;
 	};
 
 	var make_mapping = function(reverse) {
-		var i;
 		mapping = []
 
-		for (i = 0; i < source.length; i++) {
-			mapping[reverse[i]] = i;
-		}
+		_.each(_.range(source.length), function(v) {
+			mapping[reverse[v]] = v;
+		});
 
-		for (i = 0; i < parsed.length; i++) {
-			if (mapping[i] == undefined) {
-				mapping[i] = mapping[i-1];
+		_.each(_.range(parsed.length), function(v) {
+			if (_.isUndefined(mapping[v])) {
+				mapping[v] = mapping[v-1];
 			}
-		}
+		});
 	};
 
 	root.load_debug = function (data) {
-		var i;
-
 		source = data.code.raw.split('\n');
 		parsed = data.code.lines;
 
 		make_mapping(data.code.mapping);
 
-		for (i in source) {
-			$('#raw_code').append(make_line(i, i, source[i], source));
-		}
+		_.each(source, function(v, k) {
+			$('#raw_code').append(make_line(k, k, v, source));
+		});
 
-		for (i in parsed) {
-			var code = parsed[i].join(" ");
-			$('#parsed_code').append(make_line(i, mapping[i], code, parsed));
-		}
+		_.each(parsed, function(v, k) {
+			var code = v.join(" ");
+			$('#parsed_code').append(make_line(k, mapping[k], code, parsed));
+		});
 
-		var exe = data.stack.exe;
-		for (i in exe) {
-			$('#stack').append(make_stack(exe[i]));
-		}
+		_.each(data.stack.exe, function(v, k) {
+			$('#stack').append(make_stack(v));
+		});
 
 		$('#stack').append(make_current(data.stack.pos));
 	};
